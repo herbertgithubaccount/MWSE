5548cd074d42c0c1f39c8c736c3ce6ada48b5097
herbert100 <154224164+herbertgithubaccount@users.noreply.github.com>
2024-04-30 07:01:45 +0200

    tweak to inpute config documentation

--- autocomplete/definitions/namedTypes/tes3inputConfig/code.lua
+++ autocomplete/definitions/namedTypes/tes3inputConfig/code.lua
@@ -1,5 +1,5 @@
 return {
 	type = "value",
-	description = [[The scan or input button code for the binding. Device specific. For keyboards, this is a scan code.]],
+	description = [[The scan or input button code for the binding. Device specific. For keyboards, this is a [`tes3.scanCode`](../references/scan-codes.md). For mice, this is the mouse button: `0`, `1`, `2`, correspond to left click, right click, and middle click, respectively. (Other mouse buttons are also supported, and correspond to values `> 2`.)]],
 	valuetype = "number",
 }
--- autocomplete/definitions/namedTypes/tes3inputConfig/device.lua
+++ autocomplete/definitions/namedTypes/tes3inputConfig/device.lua
@@ -1,5 +1,5 @@
 return {
 	type = "value",
-	description = [[The index of the device used in the keybinding, such as keyboard, mouse, joystick, or gamepad.]],
+	description = [[The index of the device used in the input config. Could be a keyboard, mouse, joystick, or gamepad. Keyboards have index `0` and mice have index `1`.]],
 	valuetype = "number",
 }
