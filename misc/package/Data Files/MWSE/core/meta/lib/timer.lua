89f241c22528a3b485cab2bf4e4a7096ebe2dd6e
C3pa <41503714+C3pa@users.noreply.github.com>
2025-06-10 15:30:43 +0200

    Docs: correct type of color properties of tes3uiTextSelect
    Mention persistent timers in the warning at the top of the timer page

--- misc/package/Data Files/MWSE/core/meta/lib/timer.lua
+++ misc/package/Data Files/MWSE/core/meta/lib/timer.lua
@@ -6,7 +6,7 @@
 --- The timer library provides helper functions for creating delayed executors.
 --- 
 --- !!! warning "Timers get canceled when loading saves."
---- 	All active timers will be canceled right before the [`loaded`](../events/loaded.md) event triggers.
+--- 	All active timers will be canceled right before the [`loaded`](../events/loaded.md) event triggers. Consider using a [persistent timer](https://mwse.github.io/MWSE/apis/timer/#timerregister) if you need it to persist between game sessions.
 --- 
 --- @class timerlib
 --- @field active any Constant to represent a timer that is actively running.
