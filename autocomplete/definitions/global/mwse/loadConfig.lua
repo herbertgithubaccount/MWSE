1bb86181cdd3fbbe086bd8c48a8af2013da491fe
Virnetch <49251900+Virnetch@users.noreply.github.com>
2025-06-11 19:22:43 +0300

    Use generics in `mwse.loadConfig`

--- autocomplete/definitions/global/mwse/loadConfig.lua
+++ autocomplete/definitions/global/mwse/loadConfig.lua
@@ -8,9 +8,12 @@ If the default values table is passed:
  - If no file exists, the function will return the default table.
  - In json, tables can be either arrays with integer keys or dictionaries with string keys. If your configuration table is mixed (has both string and integer indices), saving and loading from json will effectively convert all your integer indices to strings. This function will convert your configuration table's integer indices back if defaults table is given.
 ]],
+	generics = {
+		{ name = "configType", inherits = "table" }
+	},
 	arguments = {
 		{ name = "fileName", type = "string", description = "The non-extensioned name of the config file." },
-		{ name = "defaults", type = "table", optional = true, description = "A table of default values." },
+		{ name = "defaults", type = "configType", optional = true, description = "A table of default values." },
 	},
-	returns = "table",
+	returns = "configType",
 }
