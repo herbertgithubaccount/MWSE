de4b4cec665caf65949a853c78f058d8a8c24dd1
Virnetch <49251900+Virnetch@users.noreply.github.com>
2025-06-11 21:34:03 +0300

    Docs: Fix `tes3ui.showMessageMenu.params.tooltip` not marked optional

--- autocomplete/definitions/namedTypes/tes3ui.showMessageMenu.params.tooltip/text.lua
+++ autocomplete/definitions/namedTypes/tes3ui.showMessageMenu.params.tooltip/text.lua
@@ -1,5 +1,5 @@
 return {
 	type = "value",
 	description = [[The text in the body of the tooltip. Can also be a function that returns a string.]],
-	valuetype = "string|fun(callbackParams: table): string|nil",
+	valuetype = "nil|string|fun(callbackParams: table): string|nil",
 }
