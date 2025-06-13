6bae562b87652d01201a43be540360b94778b572
Virnetch <49251900+Virnetch@users.noreply.github.com>
2025-06-13 22:17:51 +0300

    Docs: Update `logger:assert` to match implementation
    - Uses generics for the return value
    - Message param also accepts a function similar to `mwseLogger:error`
    - Edited the description to more closely match `assert`

--- autocomplete/definitions/namedTypes/mwseLogger/assert.lua
+++ autocomplete/definitions/namedTypes/mwseLogger/assert.lua
@@ -1,9 +1,16 @@
 return {
 	type = "method",
-	description = [[Assert a condition and log an error if it fails.]],
+	description = [[Logs and raises an error if the value of its argument v is false (i.e., `nil` or `false`); otherwise, returns all its arguments.]],
+	generics = {
+		{ name = "T" },
+	},
 	arguments = {
-		{ name = "condition", type = "boolean" },
-		{ name = "message", type = "string" },
-		{ name = "...", type = "any", description = "Formatting arguments. These are passed to `string.format`.", optional = true },
+		{ name = "v", type = "T", optional = true },
+		{ name = "message", type = "string|fun(...): ..." },
+		{ name = "...", type = "any", description = "Formatting arguments. These are passed to `string.format`. Tables and userdata values are pretty printed.", optional = true },
+	},
+	returns = {
+		{ name = "v", type = "T" },
+		{ name = "...", type = "any" },
 	}
 }
